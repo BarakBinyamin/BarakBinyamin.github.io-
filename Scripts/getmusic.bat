87153ad6ae4c21f1da7eb95784c61a4ee58bc331
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-03 14:02:41 -0400

    Update getmusic.bat

--- Scripts/getmusic.bat
+++ Scripts/getmusic.bat
@@ -4,7 +4,7 @@
 :: In CMD C://pip install youtube-dl
 
 :: FFmpeg, converts to mp3, not essential
-:: get ffmpeg at https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip >  move into a temp folder and unzip > make a folder in C:\program files name ffmpeg >
+:: get ffmpeg at https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip >  move into a temp folder and unzip > make a folder in "C:\program files" name ffmpeg >
 :: move unzipped contents into the ffmpeg folder > find the bin folder in the ffmpeg > cut all contents into the ffmpeg folder and delete the other long path > open settings
 :: Advanced System Settings > Advanced tab > Environment Variables > select Path in the second table > edit > new > paste the path to the bin folder C:\Program Files\ffmpeg\
 :: save, now you can use ffmpeg for other programs too... https://video.stackexchange.com/questions/20495/how-do-i-set-up-and-use-ffmpeg-in-windows
