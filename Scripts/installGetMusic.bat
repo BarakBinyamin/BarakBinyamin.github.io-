4e0a1ed595ed5944c7eeb1c7f3442c6391118162
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-09 19:17:59 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -2,8 +2,9 @@
 Echo "please wait until the CMD window disappears"
 Echo ""
 pip install youtube-dl
-cd C:\Program Files
+
 powershell -Command "
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip', 'ffmpeg.zip')"
 powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip -OutFile ffmpeg.zip"
 powershell "Expand-Archive -Force 'C:\Users\rocky\Desktop\song\ffmpeg.zip' 'C:\Program Files\ffmpeg'"
+cd C:\Program Files
