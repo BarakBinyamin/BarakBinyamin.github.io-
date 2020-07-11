7ff904fd75463f99ae0b8c8f84c9c433f01de55b
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-11 13:47:13 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -10,6 +10,8 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zer
 powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip -OutFile ffmpeg.zip"
 powershell -Command "New-Item -ItemType directory Path 'C:\Program Files\ffmpeg'"
 powershell "Expand-Archive -Force 'C:\Users\%username%\Desktop\getmusic\ffmpeg.zip' 'C:\Program Files\ffmpeg'"
+cd "C:\Program Files\ffmpeg\ffmpeg-2020200628-4cfcfb3-win64-static"
+xcopy /e /v 'C:\Program Files\ffmpeg\ffmpeg-2020200628-4cfcfb3-win64-static' 'C:\Program Files\ffmpeg'
 
 ::Downloading program
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://barakbinyamin.github.io/Scripts/installGetMusic.bat', 'getmusic.bat')"
@@ -17,5 +19,8 @@ powershell -Command "Invoke-WebRequest https://barakbinyamin.github.io/Scripts/i
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://barakbinyamin.github.io/Scripts/YOUTUBELINKS.txt', 'YOUTUBELINKS.txt')"
 powershell -Command "Invoke-WebRequest https://barakbinyamin.github.io/Scripts/YOUTUBELINKS.txt -OutFile YOUTUBELINKS.txt"
 
-::move stuff directly into ffmpeg
-::pathman ffmpeg
+pathman /as 'C:\Program Files\ffmpeg\bin'
+
+echo "download complete, press any key to exit"
+pause
+exit
