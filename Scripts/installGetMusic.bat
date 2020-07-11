dcf8517f64d2cb6b10c57e164380191204662c94
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-11 15:36:40 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -1,9 +1,18 @@
 :: Install dependecies  
 Echo please wait until the CMD window disappears  
 pause  
-pip install youtube-dl  
+  
 powershell -Command "New-Item -ItemType directory -Path 'C:\Users\%username%\Desktop\getmusic'"  
 cd C:\Users\%username%\Desktop\getmusic  
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.8.4/python-3.8.4rc1-embed-amd64.zip', 'python.zip')"   
+powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.8.4/python-3.8.4rc1-embed-amd64.zip -OutFile python.zip" 
+powershell "Expand-Archive -Force 'C:\Users\%username%\Desktop\getmusic\python.zip' 'python.exe'"
+C:\Users\%username%\Desktop\getmusic\python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"   
+powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py" 
+python get-pip.py
+pip install youtube-dl
+
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip', 'ffmpeg.zip')"  
 powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip -OutFile ffmpeg.zip"  
 powershell -Command "New-Item -ItemType directory -Path 'C:\Program Files\ffmpeg'"  
