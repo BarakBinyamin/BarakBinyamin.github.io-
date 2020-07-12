66a7737e5aee4e0b3b3a71ff456e75b4a483ad30
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-12 17:06:46 -0400

    Update goto.js

--- BinShortcut/goto.js
+++ BinShortcut/goto.js
@@ -1,6 +1,12 @@
+url = "https://clbin.com/"
 function goto(){
  url = "https://clbin.com/" + document.getElementById("code").value;
  window.open(url, '_blank')
+ window.setTimeout(click(), 500)
+}
+
+function click(){
+document.getElementById('blank').click()
 }
 
 //attaching events to keys
