a3f71c814f13733a721a20c875952ba948f92f1b
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-12 11:54:37 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -17,7 +17,7 @@ cd C:\Users\%username%\Desktop\getmusic
 
 ::install youtube-dl
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yt-dl.org/downloads/2020.06.16.1/youtube-dl.exe', 'youtube-dl.exe')"   
-powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe -OutFile youtube-dl.exe"
+powershell -Command "Invoke-WebRequest https://yt-dl.org/downloads/2020.06.16.1/youtube-dl.exe -OutFile youtube-dl.exe"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe', 'vcredist_x86.exe')"   
 powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe -OutFile vcredist_x86.exe"
 vcredist_x86.exe
@@ -38,6 +38,6 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://barakbinya
 powershell -Command "Invoke-WebRequest https://barakbinyamin.github.io/Scripts/YOUTUBELINKS.txt -OutFile YOUTUBELINKS.txt"  
 
 
-echo download complete, press any key to exit  
+echo download complete, press any key to exit...
 pause  > NUL
 exit  
