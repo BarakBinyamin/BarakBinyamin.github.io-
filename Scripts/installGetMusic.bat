d23f537ff3088fe5e3777b6b37065728affb7d47
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-12 11:36:05 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -19,7 +19,7 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yt-dl.org/
 powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe -OutFile youtube-dl.exe"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe', 'vcredist_x86.exe')"   
 powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe -OutFile vcredist_x86.exe"
-
+vcredist_x86.exe
 
 ::install and configure ffmpeg
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip', 'ffmpeg.zip')"  
