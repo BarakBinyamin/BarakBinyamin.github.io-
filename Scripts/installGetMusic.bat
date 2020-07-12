146757cec91dcc39c26dad920a7b7211d2ebce60
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-12 11:20:36 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -12,12 +12,13 @@ cd C:\Users\%username%\Desktop\getmusic
 ::powershell -Command "(New-Object Net.WebClient).DownloadFile('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"   
 ::powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py" 
 ::python get-pip.py
+::setx path "%path%;'C:\Program Files\ffmpeg\bin'"  
 
 ::install youtube-dl
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yt-dl.org/downloads/2020.06.16.1/youtube-dl.exe', 'youtube-dl.exe')"   
 powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe -OutFile youtube-dl.exe"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.microsoft.com/en-US/download/confirmation.aspx?id=5555', 'visualc.exe')"   
-powershell -Command "Invoke-WebRequest https://www.microsoft.com/en-US/download/confirmation.aspx?id=5555 -OutFile visualc.exe"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe', 'vcredist_x86.exe')"   
+powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe -OutFile vcredist_x86.exe"
 
 
 ::install and configure ffmpeg
@@ -25,8 +26,9 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zer
 powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip -OutFile ffmpeg.zip"  
 powershell -Command "New-Item -ItemType directory -Path 'C:\Program Files\ffmpeg'"  
 powershell "Expand-Archive -Force 'C:\Users\%username%\Desktop\getmusic\ffmpeg.zip' 'C:\Program Files\ffmpeg'" 
-XCOPY  \E  \I 'C:\Program Files\ffmpeg\ffmpeg-20200628-4cfcfb3-win64-static' 'C:\Program Files\ffmpeg'
-setx path "%path%;'C:\Program Files\ffmpeg\bin'"   
+XCOPY  \E  "C:\Program Files\ffmpeg\ffmpeg-20200628-4cfcfb3-win64-static" "C:\Users\%username%\Desktop\getmusic"
+
+ 
 
 ::Download getmusic script
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://barakbinyamin.github.io/Scripts/installGetMusic.bat', 'getmusic.bat')"   
