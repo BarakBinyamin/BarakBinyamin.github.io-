f5687033eb925dbf9cfdd11a6986be37fc60c416
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-12 11:59:47 -0400

    Update installGetMusic.bat

--- Scripts/installGetMusic.bat
+++ Scripts/installGetMusic.bat
@@ -19,13 +19,16 @@ cd C:\Users\%username%\Desktop\getmusic
 ::setx path "%path%;'C:\Program Files\ffmpeg\bin'"  
 
 ::install youtube-dl
+echo downloading youtube-dl...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yt-dl.org/downloads/2020.06.16.1/youtube-dl.exe', 'youtube-dl.exe')"   
 powershell -Command "Invoke-WebRequest https://yt-dl.org/downloads/2020.06.16.1/youtube-dl.exe -OutFile youtube-dl.exe"
+echo downloading visualStudio C++...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe', 'vcredist_x86.exe')"   
 powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe -OutFile vcredist_x86.exe"
 vcredist_x86.exe
 
 ::install and configure ffmpeg
+echo downloading ffmpeg...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip', 'ffmpeg.zip')"  
 powershell -Command "Invoke-WebRequest https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20200628-4cfcfb3-win64-static.zip -OutFile ffmpeg.zip"  
 powershell -Command "New-Item -ItemType directory -Path 'C:\Program Files\ffmpeg'"  
@@ -35,12 +38,14 @@ XCOPY  \E  "C:\Program Files\ffmpeg\ffmpeg-20200628-4cfcfb3-win64-static" "C:\Us
  
 
 ::Download getmusic script
+echo downloading Getmusic...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://barakbinyamin.github.io/Scripts/installGetMusic.bat', 'getmusic.bat')"   
 powershell -Command "Invoke-WebRequest https://barakbinyamin.github.io/Scripts/installGetMusic.bat -OutFile getmucic.bat"  
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://barakbinyamin.github.io/Scripts/YOUTUBELINKS.txt', 'YOUTUBELINKS.txt')"  
 powershell -Command "Invoke-WebRequest https://barakbinyamin.github.io/Scripts/YOUTUBELINKS.txt -OutFile YOUTUBELINKS.txt"  
 
-
-echo download complete, press any key to exit...
+echo download complete
+echo you can find the getmusic program in the getmusic folder on your desktop
+echo press any key to exit...
 pause  > NUL
 exit  
