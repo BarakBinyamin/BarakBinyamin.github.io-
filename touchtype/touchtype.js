51f23188df94797b078fee2975dc3c644d2703db
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-07-19 20:13:01 -0400

    Add files via upload

--- touchtype/touchtype.js
+++ touchtype/touchtype.js
@@ -1,8 +1,9 @@
 
-GlobalLetter = "B";
+GlobalLetter = "A";
 HighScore = 0;
 Score = 0;
 refreshLetter();
+
 function refreshLetter(){
 GlobalLetter = String.fromCharCode(65+Math.floor(Math.random() * 26));
 document.getElementById("display").innerHTML = GlobalLetter;
@@ -13,6 +14,7 @@ Score= Score + 1;
 if (Score>HighScore){
 HighScore = HighScore + 1;
 }
+
 document.getElementById("score").innerHTML = "Score: " + Score + " " + "High Score: " + HighScore;
 }
 
@@ -25,20 +27,29 @@ document.onkeypress = function(evt) {
    evt = evt || window.event;
    var charCode = evt.which || evt.keyCode;
    var charStr = String.fromCharCode(charCode);
-   //console.log(charStr)
    if (charStr.toUpperCase() == GlobalLetter){
+        FlashBackground("green");
 	refreshLetter();
 	console.log("Well done! The global letter is now: " + GlobalLetter.toUpperCase());
 	AddScore();
    }
    else{
+	FlashBackground("red");
 	ZeroScore();
   }
    
 };
 
+function FlashBackground(color){
+var backgroundColor = document.body.style.background;
+document.body.style.background = color;
+setTimeout(function(){ document.body.style.transition = "background 500ms"; }, 50);
+setTimeout(function(){ document.body.style.background = backgroundColor; }, 100);
+setTimeout(function(){ document.body.style.transition = ""; }, 200);
+}
+
+
 function ChangeTheme(){
-if (document.body.className== ''){
-document.body.className= "night dark-scheme";}
-else{document.body.className= ''}
+if ( document.body.className== "default" ){ document.body.className= "night"; }
+else{ document.body.className =  "default" }
 }
