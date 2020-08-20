4ab127952bf48e01c691151aa8f0275e35fb7e5e
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-08-20 16:10:52 -0400

    Add files via upload

---
+++ Scripts/EmailListFilter.sh
@@ -0,0 +1,13 @@
+#This scripts adds an " OR" on every line 
+#This script was intended to make it easey to make a filter for a large amount of emails at a time
+#Example: For a class list
+
+#usage: Bash EmailListFiler.sh EMAILLISTFILE
+
+EmailList=$1
+
+sed -i s/$/,/ $EmailList #add "," to end of every file
+sed 's/,/ /g' $EmailList > "EmailList4.txt" #replace every "," with space & redirect to new file
+sed -i s/$/OR/ "EmailListOR.txt" #add "OR" to end of every file of new file
+
+echo Look for the "EmailListOR.txt" in your folder
